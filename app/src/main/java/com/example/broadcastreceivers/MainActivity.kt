bc334a8f3d40a105dcc752098cdd9ff4c3401710
Timur <timurka.askarov@inbox.ru>
2025-03-17 00:40:37 +0300

    12.1 Создание и регистрация ресиверов

--- app/src/main/java/com/example/broadcastreceivers/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceivers/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.broadcastreceivers
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,8 +9,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MyReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_LOW)
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
---
+++ app/src/main/java/com/example/broadcastreceivers/MyReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.broadcastreceivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when(intent?.action) {
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
